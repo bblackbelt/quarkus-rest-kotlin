88f66c3dd7f6cc631c1aad85f69d92dab52e2cd3
bblackbelt <emanuele.g@gmail.com>
2021-09-19 14:04:34 +0200

    Add health endpoint

--- src/main/kotlin/org/acme/rest/repository/model/IdentityEntity.kt
+++ src/main/kotlin/org/acme/rest/repository/model/IdentityEntity.kt
@@ -1,9 +1,12 @@
 package org.acme.rest.repository.model
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase
 import javax.persistence.Entity
+import javax.persistence.Id
 import javax.persistence.IdClass
+import javax.persistence.Table
 
 @Entity
+@Table(name = "Identity")
 @IdClass(IdentityPK::class)
-class IdentityEntity(val name: String, val surname: String) : PanacheEntity()
+class IdentityEntity(@Id val email: String, val name: String, val surname: String) : PanacheEntityBase()
--- src/main/kotlin/org/acme/rest/repository/model/IdentityPK.kt
+++ src/main/kotlin/org/acme/rest/repository/model/IdentityPK.kt
@@ -1,6 +1,9 @@
 package org.acme.rest.repository.model
 
 import java.io.Serializable
+import javax.persistence.Embeddable
+import javax.persistence.Id
 import javax.validation.constraints.Email
 
-data class IdentityPK(@Email val email: String) : Serializable
+@Embeddable
+data class IdentityPK(@Id val email: String) : Serializable
